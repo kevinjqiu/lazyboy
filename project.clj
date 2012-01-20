25b30852b05119e1e4ae7bcb441c7d8ae5302c52
Kevin Qiu <kevin@freshbooks.com>
2012-01-19 22:36:43 -0500

    added webnoir and route to start netflix.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[clojure "1.3.0"]
                  [clj-webdriver "0.5.0-alpha4"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.clojure/data.json "0.1.1"]]
+                 [org.clojure/data.json "0.1.1"]
+                 [noir "1.2.2"]]
   :main lazyboy.main)
--- src/lazyboy/main.clj
+++ src/lazyboy/main.clj
@@ -1,18 +1,24 @@
 (ns lazyboy.main
   (:use [clojure.java.io :only (reader file)]
         [clojure.tools.cli]
-        [clojure.data.json :only (read-json)])
-  (:require [lazyboy.plugins.netflix :as netflix]))
+        [clojure.data.json :only (read-json)]
+        [noir.core :only (defpage)])
+  (:require [lazyboy.plugins.netflix :as netflix]
+            [noir.server :as server]))
+
+(def options (ref {}))
 
 (defn- parse-args [args]
   (cli args
        ["-f" "--config-file"]))
 
+(defpage "/start-netflix" []
+  (binding [netflix/*username* (:username @options)
+            netflix/*password* (:password @options)]
+    (netflix/login)))
+
 (defn -main [& args]
-  (let [options (first (parse-args args))
-        cfg-file (:config-file options)]
-    (with-open [cfg-reader (reader (:config-file options))]
-      (let [netflix-option (read-json cfg-reader)]
-        (binding [netflix/*username* (:username netflix-option)
-                  netflix/*password* (:password netflix-option)]
-          (netflix/login))))))
+  (let [parsed-opts (first (parse-args args))]
+    (dosync
+      (ref-set options (read-json (reader (:config-file parsed-opts)))))
+    (server/start 9999)))
