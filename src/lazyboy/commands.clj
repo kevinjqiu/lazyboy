d3334001513614572f14587b6c8655ab5fc4613d
Kevin Qiu <kevin@freshbooks.com>
2012-02-09 08:30:21 -0500

    use 'atom' to handle 'global' state

--- src/lazyboy/commands.clj
+++ src/lazyboy/commands.clj
@@ -1,9 +1,16 @@
 (ns lazyboy.commands)
 
-(declare command-map)
+;(declare command-map)
+(def command-map (atom {}))
+
+(defn get-command [command-str]
+  ((keyword command-str) @command-map))
+
+(defn add-command [command-keyword handler]
+  (swap! command-map assoc command-keyword handler))
 
 (defn dispatch [command args]
-  (let [command-handler ((keyword command) command-map)]
+  (let [command-handler (get-command command)]
     (command-handler args)))
 
 (defn- mouse-move [args]
@@ -11,5 +18,7 @@
         y (:y args)]
     {:response {:command "mouse-move" :x x :y y}}))
 
-(def command-map 
-  {:mouse-move mouse-move})
+(add-command :mouse-move mouse-move)
+
+;(def command-map 
+;  {:mouse-move mouse-move})
