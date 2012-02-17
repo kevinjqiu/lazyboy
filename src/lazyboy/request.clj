3bc86816bce7525c22c774dd125bebb58c5b6420
Kevin Qiu <kevin@freshbooks.com>
2012-02-17 08:50:48 -0500

    using macro for command registry is kind of an overkill.

--- src/lazyboy/request.clj
+++ src/lazyboy/request.clj
@@ -1,5 +1,9 @@
 (ns lazyboy.request
-  (:use [clojure.data.json :only (read-json)]))
+  (:use [clojure.data.json :only (read-json)]
+        [lazyboy.robot :only (mouse-move
+                               mouse-down
+                               mouse-left-click
+                               mouse-right-click)]))
 
 (defrecord Request [command args])
 
@@ -10,7 +14,45 @@
 (defprotocol IDispatch
   (dispatch [request]))
 
+(defonce command-map (atom {}))
+
+(defn get-command [command-str]
+  ((keyword command-str) @command-map))
+
+(defn add-command [command-keyword handler]
+  (swap! command-map assoc command-keyword handler))
+
+(add-command
+  :mouse-move
+  (fn [args]
+    (let [x (Integer. (:x args))
+          y (Integer. (:y args))]
+      (do
+        (mouse-move x y)
+        {:response "done"}))))
+
+(add-command
+  :mouse-down
+  (fn [args]
+    (let [x (Integer. (:x args))
+          y (Integer. (:y args))]
+      (do
+        (mouse-down x y)
+        {:response "done"}))))
+
+(add-command
+  :mouse-left-click
+  (fn [args]
+    (mouse-left-click)
+    {:response "done"}))
+
+(add-command
+  :mouse-right-click
+  (fn [args]
+    (mouse-right-click)
+    {:response "done"}))
+
 (extend-type Request
   IDispatch
   (dispatch [request]
-    (println (str "dispatching " request))))
+    ((get-command (:command request)) (:args request))))
